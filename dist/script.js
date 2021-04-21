1b556a4498322c48beec7e77ee3b84b33feff570
Nadya <nadiakomovich@gmail.com>
2021-04-21 09:22:11 +0300

    fadeToggle added

--- dist/script.js
+++ dist/script.js
@@ -533,6 +533,19 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.fadeOut = function (dura
   }
 
   return this;
+}; // fadeToggle
+
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.fadeToggle = function (duration, display, final) {
+  for (let i = 0; i < this.length; i++) {
+    if (window.getComputedStyle(this[i]).display === 'none') {
+      Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).fadeIn(duration, display, final);
+    } else {
+      Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).fadeOut(duration, final);
+    }
+  }
+
+  return this;
 };
 
 /***/ }),
@@ -600,13 +613,13 @@ __webpack_require__.r(__webpack_exports__);
 
 
 Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('#first').click(() => {
-  Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('div').eq(1).fadeOut(800);
+  Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('div').eq(1).fadeToggle(800);
 });
 Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('[data-count="second"]').click(() => {
-  Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('div').eq(2).fadeOut(800);
+  Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('div').eq(2).fadeToggle(800);
 });
 Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('button').eq(2).click(() => {
-  Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('.w500').fadeOut(800);
+  Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('.w500').fadeToggle(800);
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,13 @@ import "./lib/lib";
 import $ from "./lib/lib";
 
 $('#first').click(() => {
-    $('div').eq(1).fadeOut(800);
+    $('div').eq(1).fadeToggle(800);
 });
 
 $('[data-count="second"]').click(() => {
-    $('div').eq(2).fadeOut(800);
+    $('div').eq(2).fadeToggle(800);
 });
 
 $('button').eq(2).click(()=>{
-    $('.w500').fadeOut(800);
+    $('.w500').fadeToggle(800);
 });
